88f28db07dfaa8103ef9df55aa5fbbb4dbc67420
Viraaj Patel <xarteastmin@gmail.com>
2025-10-23 12:54:53 +0530

    Update vehicle_bot.py

--- vehicle_bot.py
+++ vehicle_bot.py
@@ -17,6 +17,12 @@ from telegram.ext import (
     ContextTypes, filters
 )
 
+from dotenv import load_dotenv
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
 # ===============================
 # ⚙️ CONFIGURATION
 # ===============================
